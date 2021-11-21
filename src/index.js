1b9098693b1c728206a57f53a40d034c32d8d239
luna <lunashoval@gmail.com>
2021-11-21 17:29:00 -0500

    src

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import createContent from "./content";
 import createMenu from "./menu";
-createContent();
-createMenu();
-/*
 //tabs area
+const container = document.querySelector('.container');
 const tabDiv = document.createElement("div");
 const tabContent = document.createElement("BUTTON");
-tabContent.classList.add("tabContent");
 const tabMenu = document.createElement("BUTTON");
-tabMenu.classList.add("tabMenu");
 tabContent.textContent = "Content";
 tabMenu.textContent = "Menu";
 tabDiv.appendChild(tabContent);
 tabDiv.appendChild(tabMenu);
 container.appendChild(tabDiv); 
-*/
