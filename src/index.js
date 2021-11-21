0716b40f7efc20be902b8d549e0cafcd74c041ab
luna <lunashoval@gmail.com>
2021-11-21 17:54:20 -0500

    src

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const contentBtn = document.createElement('BUTTON');
 menuBtn.textContent = "menu";
 contentBtn.textContent = "content";
 menuBtn.addEventListener('click', function(){
+    console.log("meny button clicked")
     container.textContent = "";
     createMenu();
 });
 contentBtn.addEventListener('click', function(){
+    console.log("content button clicked");
     container.textContent = "";
     createContent();
 });
