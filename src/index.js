a00b9d4769dcbd79debbe5b6a6ca661b40d564e2
luna <lunashoval@gmail.com>
2021-11-21 17:05:34 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("the webpack is working!");
+import createContent from "./content";
+import createMenu from "./menu";
+createContent();
+createMenu();
+
+/*
+//tabs area
+const tabDiv = document.createElement("div");
+const tabContent = document.createElement("BUTTON");
+tabContent.classList.add("tabContent");
+const tabMenu = document.createElement("BUTTON");
+tabMenu.classList.add("tabMenu");
+tabContent.textContent = "Content";
+tabMenu.textContent = "Menu";
+tabDiv.appendChild(tabContent);
+tabDiv.appendChild(tabMenu);
+container.appendChild(tabDiv); 
+*/
