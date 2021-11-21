e2b75303fb74a09e02a735930940753747a420b3
luna <lunashoval@gmail.com>
2021-11-21 18:33:20 -0500

    src

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import createContent from "./content";
 import createMenu from "./menu";
 createContent();
 //tab area
+
+
+const createBtn = function(){
+    const menuBtn = document.createElement('BUTTON');
+    const contentBtn = document.createElement('BUTTON');
+    menuBtn.textContent = "menu";
+    contentBtn.textContent = "content";
+    menuBtn.addEventListener('click', function(){
+        console.log("meny button clicked");
+        container.textContent = "";
+        createMenu();
+        createBtn();
+    });
+    contentBtn.addEventListener('click', function(){
+        console.log("content button clicked");
+        container.textContent = "";
+        createContent();
+        createBtn();
+    });
+    container.appendChild(menuBtn);
+    container.appendChild(contentBtn);
+}
+
 const menuBtn = document.createElement('BUTTON');
 const contentBtn = document.createElement('BUTTON');
 menuBtn.textContent = "menu";
@@ -10,11 +33,13 @@ menuBtn.addEventListener('click', function(){
     console.log("meny button clicked");
     container.textContent = "";
     createMenu();
+    createBtn();
 });
 contentBtn.addEventListener('click', function(){
     console.log("content button clicked");
     container.textContent = "";
     createContent();
+    createBtn();
 });
 container.appendChild(menuBtn);
 container.appendChild(contentBtn);
