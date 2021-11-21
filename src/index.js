4352ea346854fd750b589fddeda464516639e2b2
luna <lunashoval@gmail.com>
2021-11-21 17:41:15 -0500

    src

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import createContent from "./content";
 import createMenu from "./menu";
 createContent();
 //tab area
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 const menuDiv = document.createElement('BUTTON');
 const contentDiv = document.createElement('BUTTON');
 menuDiv.textContent = "menu";
 contentDiv.textContent = "content";
-body.appendChild(menuDiv);
-body.appendChild(contentDiv);
+container.appendChild(menuDiv);
+container.appendChild(contentDiv);
